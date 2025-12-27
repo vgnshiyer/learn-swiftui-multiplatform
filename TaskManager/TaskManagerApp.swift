07ef51783e5b7a40f1bca842aead2fc45a49086d
Vignesh Iyer <vgnshiyer@gmail.com>
2025-12-27 09:28:20 -0800

    Initial Commit

---
+++ TaskManager/TaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by Vignesh Iyer on 12/27/25.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TaskManagerTests/TaskManagerTests.swift
@@ -0,0 +1,16 @@
+//
+//  TaskManagerTests.swift
+//  TaskManagerTests
+//
+//  Created by Vignesh Iyer on 12/27/25.
+//
+
+import Testing
+
+struct TaskManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
